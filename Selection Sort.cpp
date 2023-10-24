7d4f12defe17e42fed5d70b5fb0ed41314f16541
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2023-10-24 22:35:44 +0300

    Create Selection Sort.cpp

---
+++ Selection Sort.cpp
@@ -0,0 +1,35 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+void selection_sort(int arr[], int n) {
+		for (int i = 0; i < n - 1; i++)
+		{
+			int index = i;
+
+			for (int j = i + 1; j < n; j++)
+				if (arr[j] < arr[index]) { //ascending = (arr[j] > arr[index])
+					index = j;
+				}
+			swap(arr[index], arr[i]);
+
+		}
+}
+
+void print(int arr[], int size)
+{
+
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+}
+
+int main() {
+
+	int arr[] = { 10, -4, 80, 30, 90,67 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	selection_sort(arr, n);
+	print(arr, n);
+	return 0;
+}
